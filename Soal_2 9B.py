1461b2cd8ba9d4e44b0139a9435a39cefdd413f7
Mizan <ghodafail@gmail.com>
2025-04-28 07:25:09 +0700

    Update and rename Soal_8.py to Soal_2 9B.py

---
+++ Soal_2 9B.py
@@ -0,0 +1,13 @@
+#Soal 2 9B
+from collections import namedtuple
+
+Orang = namedtuple("Orang", "nama anak")
+john = Orang("John Doe", ["Timmy", "Jimmy"])
+
+print(john)
+print(id(john.anak))
+
+john.anak.append("Tina")
+
+print(john)
+print(id(john.anak))
--- Soal_8.py
+++
@@ -1,6 +0,0 @@
-#Soal 8
-class mahasiswa:
-  def __init__(self, nama, nim, tgl_lahir):
-    self.nama = nama
-    self.nim = nim
-    self.tgl_lahir = tgl_lahir
